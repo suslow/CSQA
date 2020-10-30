17c1d43bf26c40103ca865c0fe13ed74dcb5b6eb
vlarsu <vlarsu4@gmail.com>
2020-10-30 21:48:04 +0300

    gui1

--- GUI.cs
+++ GUI.cs
@@ -1,17 +1,43 @@
-using Terminal.Gui;
+using Konsole;
+using Konsole.Drawing;
+using Konsole.Forms;
+using Konsole.Internal;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.ConsoleColor;
 
 namespace CSQA
 {
-    class GUI
+    public class GUI
     {
-        static void MainGUI()
+        public void Konsole()
         {
-            Application.Init();
+            var c = new Window();
+            var consoles = c.SplitRows(
+                    new Split(3, "Опрос", LineThickNess.Single),
+                    new Split(0),
+                    new Split(3, "status", LineThickNess.Single)
+            );
 
-            var n = MessageBox.Query(50, 7, "Опрос", "Начать опорос?", "Да", "Нет");
+            var headline = consoles[0];
+            var status = consoles[2];
 
+            var contents = consoles[1].SplitColumns(
+                    new Split(0, "Варианты ответов") { Foreground = ConsoleColor.White, Background = ConsoleColor.Cyan },
+                    new Split(20, "Информация")
+            );
+            var content = contents[0];
+            var sidebar = contents[1];
 
-            Application.Run();
+            headline.Write("Пройдите опрос выбираяя подходящий ответ");
+            content.WriteLine("Вариант ...");
+
+            sidebar.WriteLine("Дополнительная информация сопровождающая опрос");
+
+            status.Write("что тут");
+            Console.ReadLine();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ namespace CSQA
     {
         static void Main(string[] args)
         {
+            var gui = new GUI();
             var interview = new Interview();
             interview.InitTest();
 
             do
             {
+                gui.Konsole();
                 interview.Start();
                 interview.Result();
                 Console.WriteLine("Чтобы завершить опрос нажмите - {Q}");
