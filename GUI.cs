cd3bfe5032ab0108e29ed676e160eae0ded4fb36
vlarsu <vlarsu4@gmail.com>
2020-10-30 21:57:43 +0300

    gui1

--- GUI.cs
+++ GUI.cs
@@ -12,18 +12,32 @@ namespace CSQA
 {
     public class GUI
     {
-        static int MainGUI()
+        public void Konsole()
         {
             var c = new Window();
             var consoles = c.SplitRows(
                     new Split(3, "Опрос", LineThickNess.Single),
                     new Split(0),
                     new Split(3, "status", LineThickNess.Single)
-            );
+            ); ; ;
 
-            var n = MessageBox.Query(50, 7, "Опрос", "Начать опорос?", "Да", "Нет");
-            
-            return n;
+            var headline = consoles[0];
+            var status = consoles[2];
+
+            var contents = consoles[1].SplitColumns(
+                    new Split(0, "Варианты ответов") { Foreground = ConsoleColor.White, Background = ConsoleColor.Cyan },
+                    new Split(20, "Информация")
+            );
+            var content = contents[0];
+            var sidebar = contents[1];
+
+            headline.Write("Пройдите опрос выбираяя подходящий ответ");
+            content.WriteLine("Вариант ...");
+
+            sidebar.WriteLine("Дополнительная информация сопровождающая опрос");
+
+            status.Write("что тут");
+            Console.ReadLine();
         }
     }
 }
